b59ee2db2f1882df2129204eb6d2dbb058242ee4
danil0110 <danilmail0110@gmail.com>
2020-05-20 10:04:59 +0300

    Initial commit

---
+++ DS_IP92_LR9_ZalizchukD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DS_IP92_LR9_ZalizchukD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
